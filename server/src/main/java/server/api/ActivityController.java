b84ba0d1bf8766d9ef8b600e479d255a9e4c8153
Marcin Jarosz <m.w.jarosz@student.tudelft.nl>
2022-02-27 02:58:51 +0100

    Fixed style

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -31,7 +31,8 @@ public class ActivityController {
 
     @PostMapping(path = {"", "/"})
     public ResponseEntity<Activity> addActivity(@RequestBody Activity activity) {
-        if (isNullOrEmpty(activity.source) || !isValidUrl(activity.source) || isNullOrEmpty(activity.title) || activity.title.length() > 140 || activity.consumption <= 0) {
+        if (isNullOrEmpty(activity.source) || !isValidUrl(activity.source) || isNullOrEmpty(activity.title)
+                || activity.title.length() > 140 || activity.consumption <= 0) {
             return ResponseEntity.badRequest().build();
         }
         Activity savedActivity = activityRepository.save(new Activity(activity.title, activity.consumption, activity.source));
