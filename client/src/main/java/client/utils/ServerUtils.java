30a9a3ae49065290dac29fa1b83da58210a107f1
Joshua Gort <J.Gort@student.tudelft.nl>
2022-02-28 19:36:33 +0100

    Fixed styling of ServerUtils

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -17,10 +17,6 @@ package client.utils;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.List;
 
 import commons.Activity;
