d92f2404684a03adbfa27be02115581b24f1a836
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-03-04 13:56:44 +0100

    Added some more tests for both Question's child classes
    - Tested both equals methods in different ways to make sure that 2 object of types "QuestionHowMuch" and "QuestionMoreExpensive" are compared as intended.

--- commons/src/test/java/commons/QuestionTest.java
+++ commons/src/test/java/commons/QuestionTest.java
@@ -29,5 +29,40 @@ public class QuestionTest {
         assertEquals(question.toString(), "QuestionMultipleChoice{id=0, type=1, title='Title Test', disabledPowerUps=[], answers=[]}");
     }
 
+    @Test
+    public void EqualsMoreExpensive() {
+        Question question1 = new QuestionMoreExpensive("q1", new ArrayList<>(), new ArrayList<>());
+        Question question2 = new QuestionMoreExpensive("q1", new ArrayList<>(), new ArrayList<>());
+        assertEquals(question1, question2);
+    }
+
+    @Test
+    public void EqualsMoreExpensiveFails() {
+        Question question1 = new QuestionMoreExpensive("q1", new ArrayList<>(), new ArrayList<>());
+        Question question2 = new QuestionMoreExpensive("q2", new ArrayList<>(), new ArrayList<>());
+        assertNotEquals(question1, question2);
+    }
+
+    @Test
+    public void EqualsHowMuch() {
+        Question question1 = new QuestionHowMuch("q1", new ArrayList<>(), new ArrayList<>());
+        Question question2 = new QuestionHowMuch("q1", new ArrayList<>(), new ArrayList<>());
+        assertEquals(question1, question2);
+    }
+
+    @Test
+    public void EqualsHowMuchFails() {
+        Question question1 = new QuestionHowMuch("q1", new ArrayList<>(), new ArrayList<>());
+        Question question2 = new QuestionHowMuch("q2", new ArrayList<>(), new ArrayList<>());
+        assertNotEquals(question1, question2);
+    }
+
+    @Test
+    public void EqualsSameTitleDifferentTypesOfQuestions() {
+        Question question1 = new QuestionMoreExpensive("q1", new ArrayList<>(), new ArrayList<>());
+        Question question2 = new QuestionHowMuch("q1", new ArrayList<>(), new ArrayList<>());
+        assertNotEquals(question1, question2);
+    }
+
 
 }
