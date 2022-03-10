541c5b410f07e155f0ab41f48eee34259185b47a
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-03-10 10:49:47 +0100

    Added useful methods to RequestToJoin class + JUnit tests
    - Added equals, hashCode and toString methods (by using their equivalent builders objects, as Joshua did in previous parts of his code). 
    - Tested all the methods using JUnit tests (check RequestToJoinTest class). 
    - Fixed all indentation problems (by using auto-indentation from InteliJ). 
    - Made default constructor public (so that I could check the constructor without calling getters, since that would not be a correct JUnit test). Note that testing a constructor  could be implemented differently before making public attributes private (check ActivityTest class checkConstructor method).

--- commons/src/main/java/communication/RequestToJoin.java
+++ commons/src/main/java/communication/RequestToJoin.java
@@ -1,17 +1,22 @@
 package communication;
 
 import commons.GameInstance;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 public class RequestToJoin {
 
     private String name;
     private int gameType;
 
-    private RequestToJoin(){}
+    public RequestToJoin() {
+    }
 
     public RequestToJoin(String name, int gameType) {
         this.name = name;
-        if(gameType != GameInstance.SINGLE_PLAYER && gameType != GameInstance.MULTI_PLAYER) throw new IllegalArgumentException();
+        if (gameType != GameInstance.SINGLE_PLAYER && gameType != GameInstance.MULTI_PLAYER)
+            throw new IllegalArgumentException();
         this.gameType = gameType;
     }
 
@@ -28,7 +33,38 @@ public class RequestToJoin {
     }
 
     public void setGameType(int gameType) {
-        if(gameType != GameInstance.SINGLE_PLAYER && gameType != GameInstance.MULTI_PLAYER) throw new IllegalArgumentException();
+        if (gameType != GameInstance.SINGLE_PLAYER && gameType != GameInstance.MULTI_PLAYER)
+            throw new IllegalArgumentException();
         this.gameType = gameType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        RequestToJoin req = (RequestToJoin) o;
+
+        return new EqualsBuilder()
+                .append(name, req.name)
+                .append(gameType, req.gameType)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37)
+                .append(name)
+                .append(gameType)
+                .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("name", name)
+                .append("gameType", gameType)
+                .toString();
     }
 }
