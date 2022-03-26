673c3319091dd1bb8c886ce469b57e6534e7ab3a
Petra Postelnicu <P.Postelnicu@student.tudelft.nl>
2022-03-26 13:10:11 +0100

    pipeline fix

--- commons/src/main/java/commons/GameInstance.java
+++ commons/src/main/java/commons/GameInstance.java
@@ -1,7 +1,6 @@
 package commons;
 
 import commons.player.Player;
-import commons.player.SimpleUser;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
