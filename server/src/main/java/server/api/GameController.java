59636071f039778b9115d67ebe7b52b138aee178
Marcin Jarosz <m.w.jarosz@student.tudelft.nl>
2022-04-04 16:20:55 +0200

    Fixed pipeline

--- server/src/main/java/server/api/GameController.java
+++ server/src/main/java/server/api/GameController.java
@@ -21,10 +21,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
